e8764a79469902ae59288fe875da78576c07b3d1
namnguyxn <phuongnam3235@gmail.com>
2024-11-24 15:21:40 +0700

    feat: add product creation functionality (solution 1)

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,23 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
+Route::get('/', [HomeController::class, 'index'])
+->name('home.index');
 
-Route::get('/about', [HomeController::class, 'about'])->name('home.about');
+Route::get('/about', [HomeController::class, 'about'])
+->name('home.about');
 
-Route::get('/products', [ProductController::class, 'index'])->name('product.index');
+Route::get('/products', [ProductController::class, 'index'])
+->name('product.index');
 
-Route::get('/products/{id}', [ProductController::class, 'show'])->name("product.show");
+Route::get('/products/{id}', [ProductController::class, 'show'])
+->name("product.show");
 
-Route::get('/admin', [AdminHomeController::class, 'index'])->name('admin.home.index');
+Route::get('/admin', [AdminHomeController::class, 'index'])
+->name('admin.home.index');
 
-Route::get('/admin/products', [AdminProductController::class, 'index'])->name('admin.product.index');
+Route::get('/admin/products', [AdminProductController::class, 'index'])
+->name('admin.product.index');
+
+Route::post('/admin/products/store', [AdminProductController::class, 'store'])
+->name('admin.product.store');
